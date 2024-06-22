286b5654bfa3f738c01581c915181118972d2e80
Dayson9 <ainsteinwells@gmail.com>
2024-06-22 01:26:39 +0100

    update main.js

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -25,7 +25,13 @@ function renderUI() {
 renderUI();
 
 // Render projects UI
-renderComponent("projects", {src: "./images/pp.jpg", project_name: "Graph2Code", project_des: "A simple web app which features a simple way to design html elements and also access their equivalent HTML/CSS codes on the go.", href: "https://graph2code.vercel.app"}, "#projects");
+renderComponent("projects", 
+{
+    src: "./images/pp.jpg", 
+    project_name: "Graph2Code", 
+    project_des: "A simple web app which features a simple way to design html elements and also access their equivalent HTML/CSS codes on the go.", 
+    href: "https://graph2code.vercel.app"
+}, "#projects");
 
 renderComponent("projects", {src: "./images/pp.jpg", project_name: "QueFlowJS", project_des: "A mini JavaScript library that provides a simple way of rendering reactive state UI. Automatically reacts to changes in data and updates the DOM based on the changes.", href: "https://queflowjs.onrender.com"}, "#projects");
 
