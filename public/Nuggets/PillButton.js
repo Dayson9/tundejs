f448b7bcbe4e8baa5e162813d4f0dedfa767bc8f
dayson9 <ainsteinwells@gmail.com>
2024-11-18 20:42:37 +0100

    update

--- public/Nuggets/PillButton.js
+++ public/Nuggets/PillButton.js
@@ -3,8 +3,8 @@ import { Nugget } from 'queflow';
 const PillButton = new Nugget("PillButton", {
   template: () => {
     return `
-      <svg width="100" height="100" onclick='{{ click }}'>
-        <path d="M10,15 L85,15 C 85 15 95 35 85 50 L10,50 C 10 50 0 35 10 15 Z" fill='rgb(5,11,17)' stroke='rgb(50, 120, 180)' stroke-width='3.5' pathLength='100' stroke-dasharray="100" stroke-dashoffset="{{ stroke0 }}">
+      <svg width="100" height="100" onclick='{{ click }}' display='{{ display }}'>
+        <path d="M10,15 L85,15 C 85 15 95 35 85 50 L10,50 C 10 50 0 35 10 15 Z" fill='rgb(5,11,17)' stroke='rgb(50, 120, 180)' stroke-width='3.5' pathLength='100' stroke-dasharray="100" stroke-dashoffset="100">
             <animate attributeName="stroke-dashoffset"
                      from="100"
                      to="0"
@@ -12,7 +12,7 @@ const PillButton = new Nugget("PillButton", {
                      begin="{{ delay0 }}s"
                      fill="freeze"/>
         </path>
-        <path d="M30,33 L55,33" fill='rgb(5,11,17)' stroke='rgb(50, 120, 180)' stroke-width='3.5' pathLength='50' stroke-dasharray="50" stroke-dashoffset="{{ stroke1 }}">
+        <path d="M30,33 L55,33" fill='rgb(5,11,17)' stroke='rgb(50, 120, 180)' stroke-width='3.5' pathLength='50' stroke-dasharray="50" stroke-dashoffset="50">
             <animate attributeName="stroke-dashoffset"
                      from="50"
                      to="0"
@@ -20,7 +20,7 @@ const PillButton = new Nugget("PillButton", {
                      begin="{{ delay1 }}s"
                      fill="freeze"/>
         </path>
-        <path d="M50,24 L60,33 L50,41 Z" fill='rgb(5,11,17)' stroke='rgb(50, 120, 180)' stroke-width='3.5' pathLength='50' stroke-dasharray="50" stroke-dashoffset="{{ stroke2 }}">
+        <path d="M50,24 L60,33 L50,41 Z" fill='rgb(5,11,17)' stroke='rgb(50, 120, 180)' stroke-width='3.5' pathLength='50' stroke-dasharray="50" stroke-dashoffset="50">
             <animate attributeName="stroke-dashoffset"
                      from="50"
                      to="0"
