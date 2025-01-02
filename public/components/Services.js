1be549bf228782e200ffd2ca9fbc9653dc40b32d
dayson9 <ainsteinwells@gmail.com>
2025-01-02 19:22:30 +0100

    update Services.js

--- public/components/Services.js
+++ public/components/Services.js
@@ -14,15 +14,15 @@ const Services = new subComponent('Services', {
 
           <ServiceCard { title: 'Web Optimization', summary: 'Ensuring that websites are well optimized for speed and performance accross all devices/browsers.', icon: 'bx-code-alt' } />
  
-          <ServiceCard { title: 'Web Development', summary: 'I craft outstanding websites using web technologies such as HTML, CSS, JavaScript, Vue, QueFlow.', icon: 'bx-code-alt' } />
+          <ServiceCard { title: 'Web Accessibility', summary: 'I build websites that are accessible to users with disabilities and users with low-end devices.', icon: 'bx-code-alt' } />
         
-          <ServiceCard { title: 'Web Development', summary: 'I craft outstanding websites using web technologies such as HTML, CSS, JavaScript, Vue, QueFlow.', icon: 'bx-code-alt' } />
+          <ServiceCard { title: 'Responsive UIs', summary: 'I also design UIs that responds to different screen sizes, and devices. From mobile phones to PCs to monitors.', icon: 'bx-code-alt' } />
         
-          <ServiceCard { title: 'Web Development', summary: 'I craft outstanding websites using web technologies such as HTML, CSS, JavaScript, Vue, QueFlow.', icon: 'bx-code-alt' } />
+          <ServiceCard { title: 'Clean UIs', summary: 'Designing eye-catching UIs that resonate with its purpose. While effectively focusing on small units of UIs such as buttons, forms, menus, and typography.', icon: 'bx-code-alt' } />
         
-          <ServiceCard { title: 'Web Development', summary: 'I craft outstanding websites using web technologies such as HTML, CSS, JavaScript, Vue, QueFlow.', icon: 'bx-code-alt' } />
+          <ServiceCard { title: 'Latest Trends', summary: 'By effectively staying up-to-date with the latest trends in web development, I guarantee websites built with the latest technologies/tools.', icon: 'bx-code-alt' } />
         
-          <ServiceCard { title: 'Web Development', summary: 'I craft outstanding websites using web technologies such as HTML, CSS, JavaScript, Vue, QueFlow.', icon: 'bx-code-alt' } />
+          <ServiceCard { title: 'Version Control', summary: 'Leveraging version control tools like GitHub/GitLab to work in sync with other developers in management of code changes/versions.', icon: 'bx-code-alt' } />
         
           <ServiceCard { title: 'Web Development', summary: 'I craft outstanding websites using web technologies such as HTML, CSS, JavaScript, Vue, QueFlow.', icon: 'bx-code-alt' } />
         </div>
